9b2a6d26cf1e94c4791206bb6b261e3aa1bcea65
lxy200204 <1494055716@qq.com>
2025-07-23 17:00:28 +0800

    二级分类-基础列表渲染

--- src/apis/category.js
+++ src/apis/category.js
@@ -14,10 +14,28 @@ export function getCategoryAPI(id) {
  * @return {*}
  */
 export const getCategoryFilterAPI = (id) => {
-  return httpInstance({
+  return request({
     url: "/category/sub/filter",
     params: {
       id,
     },
+  });
+};
+
+/**
+ * @description: 获取导航数据
+ * @data {
+     categoryId: 1005000 ,
+     page: 1,
+     pageSize: 20,
+     sortField: 'publishTime' | 'orderNum' | 'evaluateNum'
+   }
+ * @return {*}
+ */
+export const getSubCategoryAPI = (data) => {
+  return request({
+    url: "/category/goods/temporary",
+    method: "POST",
+    data,
   });
 };
