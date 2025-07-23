ca7f56d2cd87986f99ee6c8c74ce0ae03e163be6
lxy200204 <1494055716@qq.com>
2025-07-23 15:44:35 +0800

    二级分类-整体认识和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from "@/views/Login/index.vue";
 import Layout from "@/views/Layout/index.vue";
 import Home from "@/views/Home/index.vue";
-import Categroy from "@/views/Category/index.vue";
+import Category from "@/views/Category/index.vue";
+import SubCategory from "@/views/SubCategory/index.vue";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -18,7 +20,11 @@ const router = createRouter({
         },
         {
           path: "category/:id",
-          component: Categroy,
+          component: Category,
+        },
+        {
+          path: "category/sub/:id",
+          component: SubCategory,
         },
       ],
     },
