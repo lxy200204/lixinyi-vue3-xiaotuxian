3c6e699f361b54f5d22d052e946db57fc899106b
lxy200204 <1494055716@qq.com>
2025-07-23 16:41:02 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,3 +1,4 @@
+import httpInstance from "@/utils/http";
 import request from "@/utils/http";
 
 export function getCategoryAPI(id) {
@@ -6,3 +7,17 @@ export function getCategoryAPI(id) {
     params: { id },
   });
 }
+
+/**
+ * @description: 获取二级分类列表数据
+ * @param {*} id 分类id
+ * @return {*}
+ */
+export const getCategoryFilterAPI = (id) => {
+  return httpInstance({
+    url: "/category/sub/filter",
+    params: {
+      id,
+    },
+  });
+};
