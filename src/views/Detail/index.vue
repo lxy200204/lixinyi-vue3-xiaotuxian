66125bd6c1cd4e2f9fb8069fa0d7214827573f64
lxy200204 <1494055716@qq.com>
2025-07-30 15:47:40 +0800

    放大镜-实现大图显示以及优化

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -4,6 +4,7 @@ import ImageView from "@/components/ImageView/index.vue";
 import { getDetail } from "@/apis/detail";
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
+import XtxSku from "@/components/XtxSku/index.vue";
 
 const route = useRoute();
 const goods = ref({});
@@ -12,6 +13,11 @@ const getGoods = async () => {
   goods.value = res.result;
 };
 onMounted(() => getGoods());
+
+//sku规格被操作时
+const skuChange = (sku) => {
+  console.log(sku);
+};
 </script>
 
 <template>
@@ -84,7 +90,7 @@ onMounted(() => getGoods());
                 </dl>
               </div>
               <!-- sku组件 -->
-
+              <XtxSku :goods="goods" @change="skuChange" />
               <!-- 数据组件 -->
 
               <!-- 按钮组件 -->
