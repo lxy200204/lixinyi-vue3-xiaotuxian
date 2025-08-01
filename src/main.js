5559f8abeea78737815275d0e21a69ee935f6469
lxy200204 <1494055716@qq.com>
2025-08-01 19:11:52 +0800

    登录-pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import App from "./App.vue";
 import router from "./router";
 
@@ -13,8 +13,12 @@ import { lazyPlugin } from "@/directives";
 import { componentPlugin } from "./components";
 
 const app = createApp(App);
+const pinia = createPinia();
 
-app.use(createPinia());
+//注册持久化插件
+pinia.use(piniaPluginPersistedstate);
+
+app.use(pinia);
 app.use(router);
 app.use(lazyPlugin);
 app.use(componentPlugin);
