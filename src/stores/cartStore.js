80c772b361bb593b10d87064ab0315a550de8a90
lxy200204 <1494055716@qq.com>
2025-08-02 12:32:40 +0800

    本地购物车-头部购物车-计算逻辑

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,6 +1,6 @@
 //封装购物车模块
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 export const useCartStore = defineStore(
   "cart",
@@ -25,13 +25,22 @@ export const useCartStore = defineStore(
     //删除购物车
     const delCart = (skuId) => {
       //思路：
-      // 1.找到删除项的下标值-splice
+      //1.找到删除项的下标值-splice
       //2.使用数组的过滤方法-fliter
       const idx = cartList.value.findIndex((item) => skuId === item.skuId);
       cartList.value.splice(idx, 1);
     };
+
+    //计算属性
+    //1.总的数量 所有项的count之和
+    const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0));
+    //2.总价 所有项的count*price之和
+    const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0));
+
     return {
       cartList,
+      allCount,
+      allPrice,
       addCart,
       delCart,
     };
