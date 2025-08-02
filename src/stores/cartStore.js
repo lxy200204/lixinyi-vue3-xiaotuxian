d2fcc37b176f6c91f02957bc9f177642cd96cc1b
lxy200204 <1494055716@qq.com>
2025-08-02 13:12:25 +0800

    本地购物车-列表购物车-单选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -31,6 +31,13 @@ export const useCartStore = defineStore(
       cartList.value.splice(idx, 1);
     };
 
+    //单选功能
+    const singleCheck = (skuId, selected) => {
+      //通过skuId找到要修改的那一项 然后把它的selected修改为传过来的selected
+      const item = cartList.value.find((item) => item.skuId === skuId);
+      item.selected = selected;
+    };
+
     //计算属性
     //1.总的数量 所有项的count之和
     const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0));
@@ -43,6 +50,7 @@ export const useCartStore = defineStore(
       allPrice,
       addCart,
       delCart,
+      singleCheck,
     };
   },
   {
