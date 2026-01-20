2e880e3a02d158c5d50898470c2998a3768684a6
lxy200204 <1494055716@qq.com>
2026-01-20 15:22:35 +0800

    在导航中添加首页链接

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -12,6 +12,9 @@ const categoryStore = useCategoryStore();
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
       <ul class="app-header-nav">
+        <li class="home">
+          <RouterLink to="/">首页</RouterLink>
+        </li>
         <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
           <RouterLink active-class="active" :to="`/category/${item.id}`">{{
             item.name
