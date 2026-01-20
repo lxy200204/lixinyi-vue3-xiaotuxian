53ff29e27c64c8688e22692c7bc5e48af3709875
lxy200204 <1494055716@qq.com>
2026-01-20 16:07:26 +0800

    优化HomeCategory组件，修正RouterLink的链接地址

--- src/views/Home/components/HomeCategory.vue
+++ src/views/Home/components/HomeCategory.vue
@@ -7,8 +7,14 @@ const categoryStore = useCategoryStore();
   <div class="home-category">
     <ul class="menu">
       <li v-for="item in categoryStore.categoryList" :key="item.id">
-        <RouterLink to="/">{{ item.name }}</RouterLink>
-        <RouterLink v-for="i in item.children.slice(0, 2)" :key="i" to="/">{{ i.name }}</RouterLink>
+        <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
+        <RouterLink
+          :to="`/category/${item.id}`"
+          v-for="i in item.children.slice(0, 2)"
+          :key="i"
+          to="/"
+          >{{ i.name }}</RouterLink
+        >
         <!-- 弹层layer位置 -->
         <div class="layer">
           <h4>分类推荐 <small>根据您的购买或浏览记录推荐</small></h4>
